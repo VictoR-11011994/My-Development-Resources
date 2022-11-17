cd18239382e40f441e60ce1d1592253b5c141db2
Victor <59967883+VictoR-11011994@users.noreply.github.com>
2022-11-17 15:43:00 +0100

    Update Model-Entity.java

--- Spring Boot/Model-Entity.java
+++ Spring Boot/Model-Entity.java
@@ -9,7 +9,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "user") // if no name specified by default it will read the class name ex. "User" on DB but there might be some problems in 
-                      // Linux by reading "User" -> "U..." (uppercase), to avoid that is better to use @Table(name = "the-name-here")
+                      // Linux (phpMyAdmin) by reading "User" -> "U..." (uppercase), to avoid that is better to use @Table(name = "the-name-here") all "lowercase"
+		      // on Windows using (MySQL Workbench) it will automatically create/recognize the data with "lowercase": "user"
 public class User {
 	
 	private String name;
